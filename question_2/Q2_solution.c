334f67d511fd93b2c712f718d4dad3783106438d
Emre Alçi <87760380+WualFabre@users.noreply.github.com>
2022-04-21 17:52:49 +0300

    Update Q2_solution.c

--- question_2/Q2_solution.c
+++ question_2/Q2_solution.c
@@ -1,30 +1,21 @@
 #include <stdio.h>
 
 int main(){
+
+    char x;
+    scanf("%c",&x);
 	
-	char x;
-	
-	scanf("%c",&x);
-	
-	if (x>=97 && x<=122){
+    if (x>=97 && x<=122){
+        printf("lower");
+     }
 		
-		
-		printf("lower");
-		}
-		
-		else if (x>='A' && x<='Z'){
-			
-			printf("upper");
-		
-		}
-	else if (x>='0' && x<='9'){
-		
-		printf("digit");
-	}
-	else {
-		
-		printf("symbol");
-	}
-	
-	
+     else if (x>='A' && x<='Z'){
+	printf("upper");
+     }
+     else if (x>='0' && x<='9'){
+	printf("digit");
+     }
+     else {
+         printf("symbol");
+     }
 }
