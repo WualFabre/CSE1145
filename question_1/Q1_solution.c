938661fe490a5904c69eb9cef3fbc3a58a67c4c2
Emre Alçi <87760380+WualFabre@users.noreply.github.com>
2022-04-21 17:55:05 +0300

    Update Q1_solution.c

--- question_1/Q1_solution.c
+++ question_1/Q1_solution.c
@@ -6,29 +6,19 @@ int main(void)
     int second_letter;
     
      scanf("%d",&number);
-   
-    
      if (number>=1 && number<=26){
-     	
      	number=number+64;
-     	
-     	printf("%c",number);
+        printf("%c",number);
 	 }
-   else if (number>26 && number<702){
-   
-   	
-   	first_letter=number/26;
-   	second_letter=number%26;
-   	
-   	first_letter=first_letter+64;
-   	second_letter=second_letter+64;
-   	
-   	printf("%c%c",first_letter,second_letter);
-   	
-   }
-   else{
-   	
-   	printf("ZZ");
+     else if (number>26 && number<702){
+         first_letter=number/26;
+   	 second_letter=number%26;
+         first_letter=first_letter+64;
+   	 second_letter=second_letter+64;
+   	 printf("%c%c",first_letter,second_letter);
+      }
+    else{
+        printf("ZZ");
    }
     return 0;
 }
