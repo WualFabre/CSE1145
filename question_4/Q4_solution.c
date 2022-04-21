9d7c5b682c488e1af5c844f058840dea17f90b4b
Emre Alçi <87760380+WualFabre@users.noreply.github.com>
2022-04-21 17:55:50 +0300

    Update Q4_solution.c

--- question_4/Q4_solution.c
+++ question_4/Q4_solution.c
@@ -1,24 +1,14 @@
 #include <stdio.h>
 
 int main(){
-int x;
-
-scanf("%d",&x);
-
-int i , buffer;
-
-
-for(i=x-1;i>=1;i--){
-	
-	
-	if (x%i==0){
-		
-		buffer=i;
-		printf("%d ", buffer);
-		
+    int x;
+    scanf("%d",&x);
+    int i , buffer;
+    for(i=x-1;i>=1;i--){
+        if (x%i==0){
+	    buffer=i;
+	    printf("%d ", buffer);
 	}
-	
-	
-}
+     }
 }
 
